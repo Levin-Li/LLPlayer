b636c333b1f10c832444fde3b0ac44e988445422
l19918762 <491337430@qq.com>
2016-10-19 21:57:14 +0800

    commit pure project
    hello world

---
+++ LLPlayer/LLPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LLPlayer
+//
+//  Created by luoluo on 16/10/19.
+//  Copyright © 2016年 luolei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LLPlayer/LLPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LLPlayer
+//
+//  Created by luoluo on 16/10/19.
+//  Copyright © 2016年 luolei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
