f3365a332cb9145a2b05a1b0c894945386f87ef4
l19918762 <491337430@qq.com>
2016-10-19 23:26:50 +0800

    commit ffmpeg
    finish find codec

--- LLPlayer/LLPlayer/ViewController.m
+++ LLPlayer/LLPlayer/ViewController.m
@@ -7,18 +7,104 @@
 //
 
 #import "ViewController.h"
+#include <libavformat/avformat.h>
+#include <libavutil/mathematics.h>
+#include <libavutil/time.h>
+#include <libswscale/swscale.h>
 
 @interface ViewController ()
 
 @end
 
-@implementation ViewController
+@implementation ViewController{
+    AVFormatContext *pformatCtx;
+    int videoIndex;
+    AVCodecContext *pCodecCtx;
+    AVCodec *pCodec;
+    AVFrame *pFrame,*pFrameYUV;
+    uint8_t *out_buffer;
+    AVPacket *packet;
+    int y_size;
+    int ret,got_picture;
+    struct SwsContext *img_convert_ctx;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self getFrameYUV];
 }
 
+-(void)getFrameYUV
+{
+    NSString *input_str= [NSString stringWithFormat:@"resource.bundle/%@",@"sintel.h264"];
+    NSString *input_nsstr=[[[NSBundle mainBundle]resourcePath] stringByAppendingPathComponent:input_str];
+    //init ffmpeg
+    av_register_all();
+    avformat_network_init();
+    pformatCtx = avformat_alloc_context();
+    // open format file to formatCtx
+    if (avformat_open_input(&pformatCtx,[input_nsstr UTF8String],NULL,NULL) != 0) {
+        printf("couldn't open input stream.\n");
+        return;
+    }
+    av_dump_format(pformatCtx, 0, [input_nsstr UTF8String], 0);
+    
+    if (avformat_find_stream_info(pformatCtx,NULL) < 0) {
+        printf("couldn't find stream information.\n");
+        return;
+    }
+    
+    
+    
+    videoIndex = -1;
+    for (int i= 0; i<pformatCtx->nb_streams; i++) {
+        if (pformatCtx->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO) {
+            videoIndex = i;
+            printf("find video stream\n");
+            break;
+        }
+    }
+    
+    if (videoIndex == -1) {
+        printf("Didn't find avideo stream");
+    }
+    
+    pCodecCtx=pformatCtx->streams[videoIndex]->codec;
+    pCodec=avcodec_find_decoder(pCodecCtx->codec_id);
+    
+    if(pCodec==NULL)
+    {
+        printf("Codec not found.\n");
+        return;
+    }
+    
+    if(avcodec_open2(pCodecCtx, pCodec,NULL)<0)
+    {
+        printf("Could not open codec.\n");
+        return;
+    }else{
+        printf("open codec.\n");
+    }
+    
+    
+    //init Frame
+    pFrame = av_frame_alloc();
+    pFrameYUV = av_frame_alloc();
+    
+    /*
+    //分配输出内存空间
+    out_buffer = (uint8_t *)av_malloc(avpicture_get_size(PIX_FMT_YUV420P, pCodecCtx->width, pCodecCtx->height));
+    avpicture_fill((AVPicture *)pFrameYUV, out_buffer, PIX_FMT_YUV420P, pCodecCtx->width, pCodecCtx->height);//分配内存空间
+    packet = (AVPacket *)av_malloc(sizeof(AVPacket));//
+    
+    //Output Info-----------------------------
+    printf("--------------- File Information ----------------\n");
+    av_dump_format(pformatCtx, 0, [input_nsstr UTF8String], 0);
+    printf("-------------------------------------------------\n");
+    img_convert_ctx = sws_getContext(pCodecCtx->width, pCodecCtx->height, pCodecCtx->pix_fmt,
+                                     pCodecCtx->width, pCodecCtx->height, PIX_FMT_YUV420P, SWS_BICUBIC, NULL, NULL, NULL);//转码的信息*/
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
