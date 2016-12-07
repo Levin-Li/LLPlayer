ae1f4c85a23b3a3395dd629c347845b55f7c9ead
l19918762 <mac@luoluo.local>
2016-12-07 18:43:21 +0800

    aac音频
    aac 音频播放部分

---
+++ LLPlayer/LLPlayer/OpenGLView/audio/LLAudioManager.h
@@ -0,0 +1,13 @@
+//
+//  LLAudioManager.h
+//  LLPlayer
+//
+//  Created by mac on 2016/12/7.
+//  Copyright © 2016年 luolei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LLAudioManager : NSObject
+
+@end
