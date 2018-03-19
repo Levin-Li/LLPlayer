ff1d4cb1b59f40071368c19072adf2132e5a990f
liguangluo <491337430@qq.com>
2018-03-19 16:28:15 +0800

    更新ffmpeg编译库

---
+++ EncodeDemo/EncodeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EncodeDemo
+//
+//  Created by luo luo on 02/03/2018.
+//  Copyright © 2018 ChangeStrong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
